1328e56dabe712d825996840adfde4e18992a062
jungsungwook <jswcyber@naver.com>
2023-01-12 17:04:22 +0900

    init

---
+++ main.py
@@ -0,0 +1,48 @@
+# pip install pypiwin32
+# pip install pyautogui
+# pip install pygetwindow
+# pip install pyMeow-1.21.8.zip
+import win32gui
+import pyautogui
+import pygetwindow as gw
+import pyMeow as pm
+
+def screenshot(window_title="MapleStory"):
+    if window_title:
+        hwnd = win32gui.FindWindow(None, window_title)
+        if hwnd:
+            win32gui.SetForegroundWindow(hwnd)
+            x, y, x1, y1 = win32gui.GetClientRect(hwnd)
+            x, y = win32gui.ClientToScreen(hwnd, (x, y))
+            x1, y1 = win32gui.ClientToScreen(hwnd, (x1 - x, y1 - y))
+            im = pyautogui.screenshot(region=(x, y, x1, y1))
+            return im
+        else:
+            print('# Window not found!')
+    else:
+        # time.sleep(0.2)
+        im = pyautogui.screenshot()
+        return im
+
+def read_offsets(proc, base_address, offsets):
+    basepoint = pm.read_int(proc, base_address)
+    current_pointer = basepoint  
+    for i in offsets[:-1]:
+        current_pointer = pm.read_int(proc, current_pointer+i)
+
+    return current_pointer + offsets[-1]
+
+def getWindows():
+    title = gw.getAllTitles()
+    print(title)
+
+if __name__ == '__main__':
+    print("start")
+    process = pm.open_process(processName="KakaoTalk.exe")
+    base =  pm.get_module(process, "KakaoTalk.exe")["base"]
+    print(process)
+    pm.overlay_init(fps=60)
+    while pm.overlay_loop():
+        pm.begin_drawing()
+        pm.draw_fps(10,10)
+        pm.end_drawing();
